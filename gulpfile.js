005a6927fd8287b3d33b316722534e79421b56dc
jemassey <jemassey@aaluu.com>
2016-01-14 10:43:45 -0800

    replace dynamic decimal calculation with a strict precision calculation, some refactoring

--- gulpfile.js
+++ gulpfile.js
@@ -1,10 +1,30 @@
 var gulp = require('gulp');
-var mocha = require('gulp-mocha');
+var jshint = require('gulp-jshint');
 var gutil = require('gulp-util');
+var babel = require('gulp-babel');
 var defineModule = require('gulp-define-module');
 
-gulp.task('default', function(){
-  return gulp.src('lib/kbn.js', {read: false})
+var JS_PATH = 'lib/*.js';
+
+//Execute  JSHINT on JS
+gulp.task('lint', function () {
+  return gulp.src(JS_PATH)
+    .pipe(jshint())
+    .pipe(jshint.reporter('default'));
+});
+
+//Build function
+gulp.task('develop-script', function () {
+  return gulp.src(JS_PATH)
+    .pipe(babel())
     .pipe(defineModule('hybrid'))
-    .pipe(gulp.dest('build/'));
+    .pipe(gulp.dest('dist'));
+});
+
+
+//Local development task. Runs through concat, jshint
+gulp.task('default', ['lint', 'develop-script'], function () {
+  // place code for your default task here
+  gulp.watch(JS_PATH, ['lint', 'develop-script']);
+  // gulp.watch(TEST_JS_PATH, ['develop-test']);
 });
